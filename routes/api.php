976d4538dd81c03789469b54db263c0338f89cb7
mellisadmyn <mellisadamayantii4@gmail.com>
2023-07-13 09:16:08 +0800

    first commit

---
+++ routes/api.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PasswordResetController;
+
+// Public Routes
+Route::post('/register', [UserController::class, 'register']);
+Route::post('/login', [UserController::class, 'login']);
+Route::post('/send-reset-password-email', [PasswordResetController::class, 'send_reset_password_email']);
+Route::post('/reset-password/{token}', [PasswordResetController::class, 'reset']);
+
+// Protected Routes
+Route::middleware(['auth:sanctum'])->group(function(){
+    Route::post('/logout', [UserController::class, 'logout']);
+});
